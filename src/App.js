2605e0271c5e34c92b342cec070b01dbfc95e0fc
Flávio Santos <flavioraph@live.com>
2021-09-03 17:10:04 -0300

    Alteration in Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import './App.css';
-import { useState } from 'react';
 import './Components/Fruits'
 import Fruits from './Components/Fruits';
 
 function App() {
-  const [fruits, setFruits] = useState([ { name: "banana", color: "yellow", price: 2 }, 
-  { name: "cherry", color: "red", price: 3 },
-  { name: "strawberry", color: "red", price: 4 }, ]);
-  const filterRedFruits = fruits.filter((item) => item.color === 'red')
-  const value = fruits.map(
-    (item)=>
-     <Fruits name={item.name}/>
-  )
-  const price = fruits.reduce((acc, item)=> item.price + acc, 0)
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <div className='price'>Price = {price}</div>
         <div>
             <ol>
-              {value}
+              <Fruits/>
             </ol>
           </div>
-        <button onClick={()=> setFruits(filterRedFruits)}>Mostra Frutas Vermelhas</button>
       </header>
     </div>
   );
--- src/Components/Fruits/index.js
+++ src/Components/Fruits/index.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
 import './style.css'
+
 function Fruits({name}){
-    return (
+    const [fruits, setFruits] = useState([ { name: "banana", color: "yellow", price: 2 }, 
+    { name: "cherry", color: "red", price: 3 },
+    { name: "strawberry", color: "red", price: 4 }, ]);
+
+    const filterRedFruits = fruits.filter((item) => item.color === 'red')
+
+    const value = fruits.map(
+      (item)=>
+       <li>{item.name}</li>
+    )
+
+    const price = fruits.reduce((acc, item)=> item.price + acc, 0)
+
+    return(
         <>
-         <li> {name}</li>
+            <div className='price'>Price = {price}</div>
+            <ol>{value}</ol>
+            <button onClick={()=> setFruits(filterRedFruits)}>Mostra Frutas Vermelhas</button>
         </>
     )
 }
